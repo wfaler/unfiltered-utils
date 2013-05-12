e35c461370f9c1b57c42d7a2060960389e4e1004
wfaler <wille.faler@gmail.com>
2013-05-12 02:30:04 +0100

    intent for username/pass signin

--- src/main/scala/com/recursivity/web/Security.scala
+++ src/main/scala/com/recursivity/web/Security.scala
@@ -4,7 +4,10 @@ import java.security.SecureRandom
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
+import unfiltered.request._
 import unfiltered.response._
+
+import unfiltered.Cookie
 
 object PBKDF2Password{
   /** Assumes that the salt unique for each user is stored together with the password derived key
@@ -32,4 +35,35 @@ trait AccessControl{
   def accessAllowed[A,B](sessionId: A)(block: => ResponseFunction[B]): ResponseFunction[B] = if(isValidSession(sessionId)) block else Unauthorized
 
   def isValidSession[A](sessionId: A): Boolean
+
+  def invalidateSession[A](sessionId: A)
+}
+
+trait SignInIntent[A] extends AccessControl{
+  import Req._
+
+  type UnfilteredIntent[A] = PartialFunction[HttpRequest[A], ResponseFunction[A]]
+
+  def cookieName: String
+
+  def userParam: String
+
+  def passParam: String
+
+  def signIn[A](username: String, password: String): Option[A]
+  
+  def sessionIntent: UnfilteredIntent[A] = {
+    case req @ GET(Path(Seg("api" :: "session" :: Nil))) & Cookies(cookies) => 
+      cookies(cookieName) map{c => if(isValidSession(c.value)) NoContent else Unauthorized} getOrElse Unauthorized
+    case req @ POST(Path(Seg("api" :: "session" :: Nil))) & Params(params) => {
+      implicit val parameters = params
+      signIn(string(userParam) getOrElse "", string(passParam) getOrElse "") map
+        {session => SetCookies(Cookie(cookieName, session.toString, path = Some("/"))) ~> NoContent} getOrElse Unauthorized
+    }
+    case req @ DELETE(Path(Seg("api" :: "session" :: Nil))) => forget
+    case req @ GET(Path(Seg("api" :: "session" :: "forget" :: Nil))) => forget
+    case _ => Pass  
+  }
+
+  private def forget = SetCookies(Cookie(cookieName, "")) ~> NoContent
 }
