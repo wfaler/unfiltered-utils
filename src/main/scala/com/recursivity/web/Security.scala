7172a1401fc2c813dce028e8e543f8750cc26656
wfaler <wille.faler@gmail.com>
2013-05-12 01:36:36 +0100

    password validation

--- src/main/scala/com/recursivity/web/Security.scala
+++ src/main/scala/com/recursivity/web/Security.scala
@@ -7,13 +7,12 @@ import javax.crypto.spec.PBEKeySpec
 import unfiltered.response._
 
 object PBKDF2Password{
-
+  /** Assumes that the salt unique for each user is stored together with the password derived key
+  */
   def authenticate(attemptedPassword: String, encryptedPass: Array[Byte], salt: Array[Byte]): Boolean = {
     java.util.Arrays.equals(encryptedPass, encryptedPassword(attemptedPassword, salt))
   }
 
-  /** Assumes that the salt unique for each user is stored together with the password derived key
-  */
   def encryptedPassword(password: String, salt: Array[Byte], iterations: Int = 20000, keyLength: Int = 160): Array[Byte] = {
     val spec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength)
     val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
